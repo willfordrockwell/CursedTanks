9818086192d59c23c8d9a69d2bb92c25d2894385
ilia <ilia@debian>
2019-08-06 18:51:54 +0300

    Task #830139
    correct codestyle

--- lib/src/Validate_Bullet.c
+++ lib/src/Validate_Bullet.c
@@ -7,46 +7,50 @@ int Validate_Bullet (char **map,            //double-ptr to map
     int x_bullet = Get_Map_From_Pix(bullet->coord.x);
     int y_bullet = Get_Map_From_Pix(bullet->coord.y);
     
-    	//check tanks;
-	switch(bullet->direct)
-	{	
-	case UP:
-		for(int i; i<4; i++)
-			if(x_bullet==Get_Map_From_Pix(tanks->coord.x+4*i)&&y_bullet==Get_Map_From_Pix(tanks->coord.y+12)){
-				bullet->coord.x = -16;
-				bullet->coord.y = -16;
-				tanks->health--;
-				return 2;
-			}			
-		break;
-	case RIGHT:
-		for(int i; i<4; i++)
-			if(x_bullet==Get_Map_From_Pix(tanks->coord.x)&&y_bullet==Get_Map_From_Pix(tanks->coord.y+4*i)){
-				bullet->coord.x = -16;
-				bullet->coord.y = -16;
-				tanks->health--;
-				return 2;
-			}			
-		break;
-	case LEFT:
-		for(int i; i<4; i++)
-			if(x_bullet==Get_Map_From_Pix(tanks->coord.x+12)&&y_bullet==Get_Map_From_Pix(tanks->coord.y+4*i)){
-				bullet->coord.x = -16;
-				bullet->coord.y = -16;
-				tanks->health--;
-				return 2;
-			}			
-		break;
-	case DOWN:
-		for(int i; i<4; i++)
-			if(x_bullet==Get_Map_From_Pix(tanks->coord.x+4*i)&&y_bullet==Get_Map_From_Pix(tanks->coord.y)){
-				bullet->coord.x = -16;
-				bullet->coord.y = -16;
-				tanks->health--;
-				return 2;
-			}			
-		break;
-	}
+        
+    switch(bullet->direct) //check tanks;
+    {    
+    case UP:
+        for(int i; i<4; i++)
+            if(x_bullet==Get_Map_From_Pix(tanks->coord.x+4*i)
+               &&y_bullet==Get_Map_From_Pix(tanks->coord.y+12)){
+                bullet->coord.x = -16;
+                bullet->coord.y = -16;
+                tanks->health--;
+                return 1;
+            }            
+        break;
+    case RIGHT:
+        for(int i; i<4; i++)
+            if(x_bullet==Get_Map_From_Pix(tanks->coord.x)
+               &&y_bullet==Get_Map_From_Pix(tanks->coord.y+4*i)){
+                bullet->coord.x = -16;
+                bullet->coord.y = -16;
+                tanks->health--;
+                return 1;
+            }            
+        break;
+    case LEFT:
+        for(int i; i<4; i++)
+            if(x_bullet==Get_Map_From_Pix(tanks->coord.x+12)
+               &&y_bullet==Get_Map_From_Pix(tanks->coord.y+4*i)){
+                bullet->coord.x = -16;
+                bullet->coord.y = -16;
+                tanks->health--;
+                return 1;
+            }            
+        break;
+    case DOWN:
+        for(int i; i<4; i++)
+            if(x_bullet==Get_Map_From_Pix(tanks->coord.x+4*i)
+               &&y_bullet==Get_Map_From_Pix(tanks->coord.y)){
+                bullet->coord.x = -16;
+                bullet->coord.y = -16;
+                tanks->health--;
+                return 1;
+            }            
+        break;
+    }
 
     switch(map[x_bullet][y_bullet])
     {
