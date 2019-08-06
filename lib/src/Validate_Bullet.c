1b63223546fce79b6dc04385a9f548e06ed37663
ilia <ilia@debian>
2019-08-06 18:02:20 +0300

    ADD check tanks

--- lib/src/Validate_Bullet.c
+++ lib/src/Validate_Bullet.c
@@ -6,8 +6,46 @@ int Validate_Bullet (char **map,            //double-ptr to map
 {
     int x_bullet = Get_Map_From_Pix(bullet->coord.x);
     int y_bullet = Get_Map_From_Pix(bullet->coord.y);
-
-    //TODO: check tanks;
+    
+    	//check tanks;
+	switch(bullet->direct)
+	case: UP
+		for(int i; i<4; i++)
+			if(x_bullet==Get_Map_From_Pix(tanks->coord.x+4*i)&&y_bullet==Get_Map_From_Pix(tanks->coord.y+12)){
+				bullet->coord.x = -16;
+				bullet->coord.y = -16;
+				tanks->health--;
+				return 2;
+			}			
+		break;
+	case: RIGHT
+		for(int i; i<4; i++)
+			if(x_bullet==Get_Map_From_Pix(tanks->coord.x)&&y_bullet==Get_Map_From_Pix(tanks->coord.y+4*i)){
+				bullet->coord.x = -16;
+				bullet->coord.y = -16;
+				tanks->health--;
+				return 2;
+			}			
+		break;
+	case: LEFT
+		for(int i; i<4; i++)
+			if(x_bullet==Get_Map_From_Pix(tanks->coord.x+12)&&y_bullet==Get_Map_From_Pix(tanks->coord.y+4*i)){
+				bullet->coord.x = -16;
+				bullet->coord.y = -16;
+				tanks->health--;
+				return 2;
+			}			
+		break;
+	case: DOWN
+		for(int i; i<4; i++)
+			if(x_bullet==Get_Map_From_Pix(tanks->coord.x+4*i)&&y_bullet==Get_Map_From_Pix(tanks->coord.y)){
+				bullet->coord.x = -16;
+				bullet->coord.y = -16;
+				tanks->health--;
+				return 2;
+			}			
+		break;
+    
 
     switch(map[x_bullet][y_bullet])
     {
