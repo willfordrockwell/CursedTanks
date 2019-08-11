e3ca96800fdb46760f940234640afff6b59a0854
TrilessFold <=>
2019-08-11 15:54:28 +0000

    Premerged commit

--- lib/src/Handling_Bullet.cpp
+++ lib/src/Handling_Bullet.cpp
@@ -1,12 +1,14 @@
 #include <cursedtanks.h>
 
 int Handling_Bullet(bullet_c *bullet,
-					 float time)
+					float time)
 {
 		float tmp = (bullet -> Get_Speed() * time);
-		if (tmp>=4.0) tmp = 3.5;
-		if (bullet -> Is_Active())
-			switch(bullet -> Get_Dir()){
+		if (tmp>=4.0) {
+			tmp = 3.5;
+		}
+		if (bullet -> Is_Active()) {
+			switch(bullet -> Get_Dir()) {
 				case UP:
 					bullet -> sprite.move(0, -(tmp));
 					return 0;
@@ -21,5 +23,7 @@ int Handling_Bullet(bullet_c *bullet,
 					return 0;
 				default:
 					return -1;
+			}
 		}
+	return 0;
 }
